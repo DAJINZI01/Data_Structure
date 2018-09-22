432a769e908747b352528115e7f196d50bae44a2
DAJINZi01 <3350608096@qq.com>
2018-09-23 01:09:18 +0800

    TopSort of LGraph

--- 04-图/06-最小生成树问题.c
+++ 04-图/06-最小生成树问题.c
@@ -20,23 +20,40 @@
  *
  *
  * Prim算法 -- 让一棵小树长大
+ * void Prim(){
+ * 	MST = {s};
+ * 	while(1){
+ *		V = 未收录顶点中dist最小者; // dist[V] = E(s, V)或正无穷 parent[s] = -1
+ *		if(这样的V不存在) break;
+ *		将V收录进MST; // dist[v] = 0;
+ *		for(V的每个邻接点W)
+ *			if(W未被收录) // dist[W] != 0
+ *				if(E(v, w) < dist[W]){
+ *					dist[w] = E(v, w);
+ *					parent[W] = V;
+ *				}
+ * 	}
+ * 	if(MST中收录的顶点不到V个)
+ * 		Error("图不连通");
+ *
+ * }// T = O(V^2) 稠密图合算
  *
  * Kruskal算法 -- 将森林合并成树
  * void Kruskal(Graph G)
  * {
  *	MST = {};
  *	while(MST 中不到v-1条边 && E中还有边){
- *		从E中取一条权重最小的边E<v, w>;
+ *		从E中取一条权重最小的边E<v, w>;// 最小堆
  *		将E<v, w>从E中删除;
- *		if(E<v, w> 不在MST中 构成回路)
+ *		if(E<v, w> 不在MST中 构成回路) // 并查集
  *			将E<v, w>加入MST;
  *		
  *		else
- *			彻底无视E<v, w>;
+ *			彻底无视E<v, w>;// 生成树不要，E中也要删除
  *	}
  *	if(MST 中不到 v-1 条边)
  *		Error("生成树不存在");
  *
- * }
+ * }// T = O(Elog(E)) 稀疏图合算
  *
  * */
--- 04-图/07-拓扑排序.c
+++ 04-图/07-拓扑排序.c
@@ -1,15 +1,16 @@
 /*
- *  AOV (Activity On Vertex) 网络
+ *  AOV (Activity On Vertex) 网络 ，所有的真实的活动是表现在顶点上的
  *  拓扑排序
  *  拓扑序：如果图中从V到W有一条有向路径，则V一定排在W之前。
  *  		满足此条件的顶点序列称为一个拓扑序
  *  获得一个拓扑序的过程就是拓扑排序
  *  AOV如果有合理的拓扑序，则必定是有向无环图（Directed Acylic Graph,DAG）
+ *  	--> V --> V必须在V开始之前结束
  *
  * */
 
 /*
- * void topSort(){
+ * void TopSort(){
  *	for(count = 0; count < V; count++){
  *		V = 未输出的入度为0的顶点;// O[V]
  *		if(这样的V不存在){
@@ -18,7 +19,7 @@
  *		}
  *
  *		输出V, 或则记录V的输出序号;
- *		for(V 的每个邻接点 W)
+ *		for(V 的每个邻接点 W) // 将V从图中抹掉
  *			indegree[W]--;
  *	}
  * }
@@ -38,7 +39,56 @@
  *	if(count !=  V){
  *	        Error("图中有回路");
  *	}
- * }
- *
+ * }// T = O(V + E)
+ * 此算法可以用来检测有向图是否DAG
  * 
  * */
+
+/********************邻接表表示的图***************************/
+#define MaxSize 1024
+typedef int Vertex;
+typedef int WeightType;
+typedef int DataType;
+typedef struct ArcNode{
+	Vertex adjVex; // 邻接点的下标
+	WeightType weight;// 权重
+	struct ArcNode *nextArc;// 指向下一条边的指针
+}ArcNode;// 边表节点
+typedef struct VNode{
+	ArcNode *firstEdge;// 指向第一条边的指针
+	DataType data;// 存放节点的数据
+}VNode; // 顶点节点
+typedef struct GNode{
+	int Nv; // 定点数
+	int Ne; // 边数
+	VNode adjList[MaxSize]; // 邻接表数组
+}LGraph; // 图
+
+int TopSort(LGraph *Graph, Vertex TopOrder[]){
+/* 对Graph进行拓扑排序,  TopOrder[]顺序存储排序后的顶点下标 */
+	int Indegree[MaxSize];
+	ArcNode *W;
+	Vertex queue[MaxSize]; int front = 0, rear = 0;
+	Vertex V;
+	/* 初始化Indegree[] */
+	for(V = 0; V < Graph->Nv; V++) Indegree[V] = 0;
+	/* 遍历图，得到Indegree[] */
+	for(V = 0; V < Graph->Nv; V++)
+		for(W = Graph->adjList[V].firstEdge; W; W = W->nextArc)
+			++Indegree[W->adjVex]; /* 对有向边<V, W->AdjV>累计终点的入度 */
+	/* 将所有入度为0的顶点入列 */
+	for(V = 0; V < Graph->Nv; V++)
+		if(Indegree[V] == 0) {rear = (rear + 1) % MaxSize; queue[rear] = V;}
+	/* 下面进入拓扑排序 */
+	int count = 0;
+	while(front != rear){
+		front = (front + 1) % MaxSize; V = queue[front]; /* 弹出一个入度为0的顶点 */	
+		TopOrder[count++] = V; /* 将之存为结果序列的下一个元素 */
+		for(W = Graph->adjList[V].firstEdge; W; W = W->nextArc)
+			if(--Indegree[W->adjVex] == 0) /* 若删除V使得W->AdjV入度为0 */
+			{rear = (rear + 1) % MaxSize; queue[rear] = V;} /* 则该顶点入列 */
+	}
+	if(count != Graph->Nv)
+		return 0; /* 说明图中有回路, 返回不成功标志 */
+	return 1;
+}
